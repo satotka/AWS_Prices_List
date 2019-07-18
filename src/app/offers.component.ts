7156a2200b9b3248357d08cd4fe36666bd231130
satotka <takaaki.satoh@gmail.com>
2019-07-18 07:23:39 +0000

    feat(offers): update ui

--- src/app/offers.component.ts
+++ src/app/offers.component.ts
@@ -2,20 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-offers',
-  template: `
-    <ons-list>
-   <ons-list-header>Format Version</ons-list-header>
-    <ons-list-item>{{offerIndex.formatVersion}}</ons-list-item>
-    <ons-list-header>Disclaimer</ons-list-header>
-    <ons-list-item>{{offerIndex.disclaimer}}</ons-list-item>
-    <span *ngFor="let item of offerIndex.offers | keyvalue">
-      <ons-list-header>{{item.key}}</ons-list-header>
-      <div *ngFor="let att of item.value | keyvalue">
-      {{att.key}} : {{att.value}}
-      </div>
-    </span>
-    </ons-list>
-    `
+  template: require('./offers.component.html')
 })
 export class OffersComponent {
   @Input() offerIndex;
